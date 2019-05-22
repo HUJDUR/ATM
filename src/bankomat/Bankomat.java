7972736a7e1688fee8e90ff8d9d1f65709a4d4d2
Nadir Hujdur <nadir.hujdur2002@gmail.com>
2019-05-22 17:22:57 +0200

    v3 - problem u serijalizaciji

--- src/bankomat/Bankomat.java
+++ src/bankomat/Bankomat.java
@@ -1,9 +1,12 @@
 package bankomat;
 
 import java.io.IOException;
+import java.io.Serializable;
 
-public class Bankomat {
+public class Bankomat implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		
 		System.out.println(" ------------/////ATM\\\\\\\\\\------------");
